e8a58a1e4f79d8f895a1c7861a1b514c323c583a
aldienjanata <aldienjanata02@gmail.com>
2026-06-19 18:02:41 +0700

    fix: remove duplicate migCard HTML, fix output visibility, clean up layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'config-all-site-v23';
+const CACHE_NAME = 'config-all-site-v24';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
