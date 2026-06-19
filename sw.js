261bcf20fee53e33ab87fe49b9347bc34fcb3b79
aldienjanata <aldienjanata02@gmail.com>
2026-06-19 16:28:28 +0700

    feat: specific VLAN selection and remove exclamation marks from migration script

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'config-all-site-v14';
+const CACHE_NAME = 'config-all-site-v15';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
