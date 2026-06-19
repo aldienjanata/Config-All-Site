68590adb72ea99c0e7c5294786fda71f0b0b47a9
aldienjanata <aldienjanata02@gmail.com>
2026-06-19 17:55:07 +0700

    fix: restore .oh header visibility and fix scroll in doGen for regular config

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'config-all-site-v22';
+const CACHE_NAME = 'config-all-site-v23';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
