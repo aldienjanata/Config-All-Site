7a405ded8e4cf8509e3917d5a900d1f520239a79
aldienjanata <aldienjanata02@gmail.com>
2026-06-19 09:47:39 +0700

    feat: add Custom tab for manual configuration

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'config-all-site-v3';
+const CACHE_NAME = 'config-all-site-v4';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
