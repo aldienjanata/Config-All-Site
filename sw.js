5659b2b65151b9bd29f08fa0ae93bbaed8477f44
aldienjanata <aldienjanata02@gmail.com>
2026-06-20 14:42:58 +0700

    fix: revert table max-height that trapped scrolling and fix scroll to bottom button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'config-all-site-v29';
+const CACHE_NAME = 'config-all-site-v30';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
