f3e8f0a39a2f2fde6a1dfa1319f5f7d2ff3322bd
aldienjanata <aldienjanata02@gmail.com>
2026-06-30 11:10:42 +0700

    feat: add manual vlan override to import modal for banyumas site

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'config-all-site-v41';
+const CACHE_NAME = 'config-all-site-v42';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
